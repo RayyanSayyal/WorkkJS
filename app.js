e923d6b48924c54f31e839529dd656b388e17596
rayyansayyal <sayyalsahab@gmail.com>
2023-10-07 18:10:19 +0500

    JSClass

---
+++ app.js
@@ -0,0 +1,19 @@
+alert  ("rayyan sayyal")
+alert  ("father sayyal")
+alert  ("grand father sayyal")
+alert  ("Age sayyal")
+alert  ("Radio sayyal")
+alert  ("Sahab sayyal")
+alert  ("rayyan sayyal")
+
+
+
+console.log  ("rayyan")
+var a = ("23")
+var b = (45)
+
+console.log (a+b*(a/b))
+
+var a =(56)
+var b = (87)
+console.log(a/b);
